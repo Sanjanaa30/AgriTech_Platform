3dda48890d850998729134702f54aab36fa324b6
sanjanaa30 <sshivanand@binghamton.edu>
2025-05-23 11:29:36 -0500

    third commit

--- frontend/krishilok/src/app/app.config.ts
+++ frontend/krishilok/src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
+  ]
 };
--- frontend/krishilok/src/app/pages/landing/landing.component.ts
+++ frontend/krishilok/src/app/pages/landing/landing.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  standalone: true,
+  imports: [TranslateModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+  }
 
+  switchLang(event: Event) {
+    const selectedLang = (event.target as HTMLSelectElement).value;
+    this.translate.use(selectedLang);
+  }
 }
